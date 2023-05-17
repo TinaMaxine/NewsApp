3ebdc89c0f0d291ec51fc999d36f6e4c143a0a74
Anjani Priyanka Neeli <anjanipriyanka01@gmail.com>
2023-05-18 01:59:38 +0530

    adding loading state

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -5,11 +5,14 @@ import "./App.css";
 function App() {
   const [users, setUsers] = useState([]);
   const [next, setNext] = useState(false);
+  const [loading, setLoading] = useState(false); // for New loading state
 
   useEffect(() => {
+    setLoading(true); // Set loading to true before making the API call
     axios.get("https://randomuser.me/api/").then((res) => {
       console.log(res.data.results);
       setUsers(res.data.results);
+      setLoading(false); // Set loading to false after receiving the data
     });
   }, [next]);
 
@@ -23,16 +26,29 @@ function App() {
         <h1>User Data</h1>
       </header>
       <main>
-        <div className="card-container">
-          {users.map((user, index) => (
-            <div key={index} className="card">
-              <h2>{user.name.first} {user.name.last}</h2>
-              <p>{user.email}</p>
-              <p>{user.gender}</p>
-              <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <p className="loading">Loading...</p> // Render the loading message when data is being fetched
+        ) : (
+          <div className="card-container">
+            {users.map((user, index) => (
+              <div key={index} className="card">
+                <h2>
+                  {user.name.first} {user.name.last}
+                </h2>
+                <p>{user.email}</p>
+                <p>{user.gender}</p>
+                <p>{user.phone}</p>
+                <p>{user.location.city}</p>
+                <p>{user.location.state}</p>
+                <p>{user.location.country}</p>
+                <img
+                  src={user.picture.large}
+                  alt={`${user.name.first} ${user.name.last}`}
+                />
+              </div>
+            ))}
+          </div>
+        )}
         <button onClick={handleNext}>Next</button>
       </main>
       <footer>
